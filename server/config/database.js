04bf1a57ef3ab39d8e194516c603e264eb29d2e0
Accuracy <arcadium.odysseyxp@gmail.com>
2024-08-07 01:13:12 -0500

    Updated config to use schema

--- server/config/database.js
+++ server/config/database.js
@@ -4,6 +4,9 @@ require('dotenv').config();
 const sequelize = new Sequelize(process.env.DB_NAME, process.env.DB_USER, process.env.DB_PASSWORD, {
   host: process.env.DB_HOST,
   dialect: 'postgres',
+  define: {
+    schema: 'cozy_corner' // Replace 'your_schema_name' with the actual schema name
+  }
 });
 
 module.exports = sequelize;
